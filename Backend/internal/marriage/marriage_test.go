a8e549259d606c1d8d6b94682f729394dc57bd42
Muhammad Muhayodin <muhayodin>
2026-09-08 01:33:04 +0500

    feat: Added Query Logic And Tests

--- Backend/internal/marriage/marriage_test.go
+++ Backend/internal/marriage/marriage_test.go
@@ -46,3 +46,59 @@ func TestCreation(t *testing.T) {
 		t.Fatalf("Error Creating New Marriage: %v", err)
 	}
 }
+
+func TestQuery(t *testing.T) {
+	ctx, driver := db.ConnectDatabase("bolt://127.0.0.1:7687")
+	defer driver.Close(ctx)
+
+	_, spouseOneId, err := person.CreateNewPerson(ctx, driver, person.NewPerson{
+		PersonName:  "Spouse One",
+		Alive:       false,
+		Gender:      person.Male,
+		DateOfBirth: "11-10-2000",
+		DateOfDeath: "11-10-2026",
+	})
+
+	if err != nil {
+		t.Fatalf("Error Creating Spouse One")
+	}
+
+	_, spouseTwoId, err := person.CreateNewPerson(ctx, driver, person.NewPerson{
+		PersonName:  "Spouse Two",
+		Alive:       false,
+		Gender:      person.Female,
+		DateOfBirth: "11-10-2000",
+		DateOfDeath: "11-10-2026",
+	})
+
+	if err != nil {
+		t.Fatalf("Error Creation Spouse Two")
+	}
+
+	id, err := CreateNewMarriage(ctx, driver, NewMarriage{
+		SpouseOne: spouseOneId,
+		SpouseTwo: spouseTwoId,
+		DateStart: "11-10-2006",
+		DateEnd:   "11-10-2026",
+	})
+
+	if err != nil || id == "" {
+		t.Fatalf("Error Creating New Marriage: %v", err)
+	}
+
+	marriages, err := GetMarriages(ctx, driver, spouseOneId)
+	valueMarriages := *marriages
+
+	if err != nil {
+		t.Fatalf("Error Getting Marriages: %v", err)
+	}
+
+	if len(valueMarriages) != 1 {
+		t.Errorf("Only One Marriage Should Have Been Here")
+	}
+
+	if valueMarriages[0].id != id {
+		t.Error("Marriage ID Should Have been the same as returned when it was created")
+	}
+
+}
--- Backend/internal/marriage/query.go
+++ Backend/internal/marriage/query.go
@@ -3,9 +3,66 @@ package marriage
 import (
 	"context"
 
+	"github.com/Muhammad9922/Generations/internal/person"
 	"github.com/neo4j/neo4j-go-driver/v6/neo4j"
 )
 
-func GetMarriages(ctx context.Context, driver neo4j.Driver, id string) {
+type QueryResponse struct {
+	id    string
+	start person.DateProper
+	end   person.DateProper
+}
 
+func GetMarriages(ctx context.Context, driver neo4j.Driver, id string) (*[]QueryResponse, error) {
+	const query = `
+	MATCH (p:Person {id: $id})-->(m: Marriage)
+	RETURN m.start AS start, m.end AS end, m.id AS id
+	`
+
+	result, err := neo4j.ExecuteQuery(ctx, driver, query, map[string]any{
+		"id": id,
+	}, neo4j.EagerResultTransformer)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []QueryResponse
+
+	records := result.Records
+
+	for _, record := range records {
+		startDateRaw, startDateFound := record.Get("start")
+		endDateRaw, endDateFound := record.Get("end")
+		idRaw, idFound := record.Get("id")
+
+		if !idFound {
+			continue
+		}
+
+		var thisResponse QueryResponse
+
+		id, ok := idRaw.(string)
+		if ok {
+			thisResponse.id = id
+		}
+
+		if startDateFound {
+			startDate, ok := startDateRaw.(string)
+			if ok {
+				thisResponse.start = person.DateProper(startDate)
+			}
+		}
+
+		if endDateFound {
+			endDate, ok := endDateRaw.(string)
+			if ok {
+				thisResponse.end = person.DateProper(endDate)
+			}
+		}
+
+		response = append(response, thisResponse)
+	}
+
+	return &response, nil
 }
