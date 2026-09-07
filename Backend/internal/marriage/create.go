3c41eb1b9a05cb8e657ddec73601f0a940bc6e7d
Muhammad Muhayodin <muhayodin>
2026-09-07 13:48:54 +0500

    Added New Marriage Struct

--- Backend/internal/marriage/create.go
+++ Backend/internal/marriage/create.go
@@ -1,14 +1,19 @@
 package marriage
 
 import (
+	"context"
+
 	"github.com/Muhammad9922/Generations/internal/person"
 )
 
 type NewMarriage struct {
 	DateStart person.DateProper
 	DateEnd   person.DateProper
+	SpouseOne string
+	SpouseTwo string
+	id        string
 }
 
-func CreateNewMarriage() {
+func CreateNewMarriage(ctx context.Context) {
 
 }
